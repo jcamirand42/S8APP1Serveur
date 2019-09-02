52048cc8e89771db76b4aa22b5ebd2248da61790
Johany Camirand <johany.camirand@usherbrooke.ca>
2019-09-02 00:06:39 -0400

    add Survey controller, survey and questions models

---
+++ APP1Serveur/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APP1Serveur.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public int SurveyId { get; set; }
+        public String Statement { get; set; }
+        public Dictionary<String, String> Choices { get; set; }
+    }
+}
