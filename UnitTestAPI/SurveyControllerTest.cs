2ba1abca7ee3d15d340d573ebe20846ce81ef144
Johany Camirand <johany.camirand@usherbrooke.ca>
2019-09-02 21:55:00 -0400

    dla marde

---
+++ UnitTestAPI/SurveyControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APP1Serveur.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTestAPI
+{
+    [TestClass]
+    public class SurveyControllerTest
+    {
+        SurveyController _controller;
+
+        public SurveyControllerTest()
+        {
+            _controller = new SurveyController();
+        }
+        [TestMethod]
+        public void TestDetailsViewData()
+        {
+            // Act
+            var okResult = _controller.Get();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+    }
+}
